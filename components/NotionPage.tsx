6f41416b54e3aec032cfc5edb8de7448e1f0d94c
flooorianflo <florian.kiem@hfg.design>
2022-11-09 10:19:03 +0100

    Arrow

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -173,6 +173,18 @@ export const NotionPage = ({
       }
     });
     subcategory.classList.add('subcategory');
+
+    // Add Arrow Div to Subcategory
+    const arrow = document.createElement('span');
+    const arrowIcon = '<div><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6 12h12.5m0 0l-6-6m6 6l-6 6" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>'
+    arrow.innerHTML = arrowIcon;
+    arrow.classList.add('arrow');
+    subcategory.appendChild(arrow);
+
+    arrow.addEventListener('click', () => {
+      console.log('arrow');
+      document.querySelector('.subcategory').scroll = 100;
+    });
     
     // After the tabs row, append the subcategory
     tabsRow.after(subcategory);
