1d730ff9c4b3401e30e72b9b305be03ffa6f2372
flooorianflo <florian.kiem@hfg.design>
2022-11-30 19:01:03 +0100

    Adjustments to Vercel

--- next.config.js
+++ next.config.js
@@ -7,11 +7,9 @@ module.exports = withBundleAnalyzer({
   env: {
     DISCORD_HOOK: process.env.DISCORD_HOOK
   },
-  images: {
-    unoptimized: true,
-  },
   staticPageGenerationTimeout: 300,
   images: {
+    unoptimized: true,
     domains: [
       'www.notion.so',
       'notion.so',
