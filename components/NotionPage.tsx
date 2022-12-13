328ef984ddee57ba73c7fe370a68bd747fad419a
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2022-12-13 17:44:41 +0100

    added underline + hover effect for signature links

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -866,7 +866,7 @@ export const NotionPage = ({
                 <a href='https://twitter.com/curationshq' target='_blank'><svg width="24px" height="24px" stroke-width="1.49" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M23 3.01s-2.018 1.192-3.14 1.53a4.48 4.48 0 00-7.86 3v1a10.66 10.66 0 01-9-4.53s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5 0-.278-.028-.556-.08-.83C21.94 5.674 23 3.01 23 3.01z" stroke="currentColor" stroke-width="1.49" stroke-linecap="round" stroke-linejoin="round"></path></svg>Twitter</a>
                 <a href='https://github.com/floriandwt/Curations' target='_blank'><svg width="24px" height="24px" stroke-width="1.49" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3" stroke="currentColor" stroke-width="1.49" stroke-linecap="round" stroke-linejoin="round"></path></svg>GitHub</a>
               </div>
-              <p>Built by <a href='https://www.antonstallboerger.com/'>Anton</a>, <a href='https://designwithtech.com/'>Florian</a> and <a href='https://www.nilseller.com/'>Nils</a></p>
+              <p className='signature'>Built by <a href='https://www.antonstallboerger.com/'>Anton</a>, <a href='https://designwithtech.com/'>Florian</a> and <a href='https://www.nilseller.com/'>Nils</a></p>
             </div>
           </div>
         </div>
