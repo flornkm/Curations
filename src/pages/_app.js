8d0a7c547d121a1a95353c29774d241f29a654a5
flooorianflo <florian.kiem@hfg.design>
2023-02-26 02:25:07 +0100

    Maintenance Mode Integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Maintenance from "@/pages/maintenance";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const maintenanceMode = true;
+
+  if (maintenanceMode) {
+    return <Maintenance />;
+  }
+
+  return <Component {...pageProps} />;
 }
