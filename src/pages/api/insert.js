bb911137849eddf7138c24b4df8114df0b6bafa5
flooorianflo <florian.kiem@hfg.design>
2023-02-27 01:37:50 +0100

    Image Uploading Update

--- src/pages/api/insert.js
+++ src/pages/api/insert.js
@@ -1,53 +1,84 @@
-import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
 import { createClient } from "@supabase/supabase-js";
 import https from "https";
 import fs from "fs";
 
+async function getBrowserInstance() {
+  const executablePath = await chromium.executablePath;
+
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: chromium.args,
+      headless: true,
+      defaultViewport: {
+        width: 1280,
+        height: 720,
+      },
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
+      width: 1280,
+      height: 720,
+    },
+    executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+}
+
 export default async (req, res) => {
-  try {
-    let { imageUrl, name, category, subcategory } = req.body;
+  let browser = null;
+  let { imageUrl, name, category, subcategory } = req.body;
 
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    );
-
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
     name = name.replace(/\s/g, "_");
+    try {
+      browser = await getBrowserInstance();
+      let page = await browser.newPage();
+      await page.goto(imageUrl);
+      const imageBuffer = await page.screenshot();
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+      const image = "uploaded_on_" + Date.now() + ".jpg";
 
-    await page.setViewport({ width: 700, height: 434, deviceScaleFactor: 2 });
-    await page.goto(imageUrl);
-    await page.screenshot({ path: "screenshot.png" });
-    await browser.close();
-
-    const image = fs.readFileSync("screenshot.png");
-
-    const { data, error } = await supabase.storage
-      .from("images")
-      .upload(`${name}.webp`, image, {
-        cacheControl: "3600",
-        upsert: false,
-        contentType: "image/webp",
-        size: 434,
-        width: 434,
-        height: 700,
-        resize: {
-          fit: "cover",
+      const { data, error } = await supabase.storage
+        .from("images")
+        .upload(`${name}.webp`, imageBuffer, {
+          cacheControl: "3600",
+          upsert: false,
+          contentType: "image/webp",
+          size: 434,
           width: 434,
           height: 700,
-        },
-      });
+          resize: {
+            fit: "cover",
+            width: 434,
+            height: 700,
+          },
+        });
 
-    if (error) {
+      if (error) {
+        console.error(error);
+        res
+          .status(500)
+          .json({ message: "Error saving screenshot to database." });
+      } else {
+        res.status(200).json({ message: "Screenshot saved to database." });
+      }
+    } catch (error) {
       console.error(error);
-      res.status(500).json({ message: "Error saving screenshot to database." });
-    } else {
-      res.status(200).json({ message: "Screenshot saved to database." });
+      res.status(500).json({ message: "Error taking screenshot." });
+    } finally {
+    if (browser !== null) {
+      await browser.close();
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error taking screenshot." });
   }
 };
