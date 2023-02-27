ff8c297fb34bfe73a448c298631b843b3dad45e7
flooorianflo <florian.kiem@hfg.design>
2023-02-27 01:44:56 +0100

    Reduced the functions even more

--- src/pages/api/insert.js
+++ src/pages/api/insert.js
@@ -1,10 +1,12 @@
 import chromium from "chrome-aws-lambda";
 import { createClient } from "@supabase/supabase-js";
+import https from "https";
 
 async function getBrowserInstance() {
   const executablePath = await chromium.executablePath;
 
   if (!executablePath) {
+    // running locally
     const puppeteer = require("puppeteer");
     return puppeteer.launch({
       args: chromium.args,
@@ -30,41 +32,54 @@ async function getBrowserInstance() {
 }
 
 export default async (req, res) => {
-  const { imageUrl, name, category, subcategory } = req.body;
+  let browser = null;
+  let { imageUrl, name, category, subcategory } = req.body;
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  name = name.replace(/\s/g, "_");
 
   try {
-    const browser = await getBrowserInstance();
-    const page = await browser.newPage();
+    browser = await getBrowserInstance();
+    let page = await browser.newPage();
     await page.goto(imageUrl);
     const imageBuffer = await page.screenshot();
 
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    );
-    const imageName = name.replace(/\s/g, "_") + ".webp";
-    const { data, error } = await supabase.storage.from("images").upload(imageName, imageBuffer, {
-      cacheControl: "3600",
-      upsert: false,
-      contentType: "image/webp",
-      size: 434,
-      width: 434,
-      height: 700,
-      resize: {
-        fit: "cover",
+    const image = "uploaded_on_" + Date.now() + ".jpg";
+
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(`${name}.webp`, imageBuffer, {
+        cacheControl: "3600",
+        upsert: false,
+        contentType: "image/webp",
+        size: 434,
         width: 434,
         height: 700,
-      },
-    });
+        resize: {
+          fit: "cover",
+          width: 434,
+          height: 700,
+        },
+      });
 
     if (error) {
       console.error(error);
-      res.status(500).json({ message: "Error saving screenshot to database." });
+      res
+        .status(500)
+        .json({ message: "Error saving screenshot to database." });
     } else {
       res.status(200).json({ message: "Screenshot saved to database." });
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error taking screenshot." });
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
   }
 };
+
