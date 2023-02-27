cd5c82a6278cf3afce56257155734475e2df11ce
flooorianflo <florian.kiem@hfg.design>
2023-02-27 03:45:19 +0100

    Inserting Update

--- src/pages/api/insert.js
+++ src/pages/api/insert.js
@@ -1,82 +1,75 @@
-import chromium from "chrome-aws-lambda";
 import { createClient } from "@supabase/supabase-js";
-import https from "https";
 import fs from "fs";
 
-async function getBrowserInstance() {
-  const executablePath = await chromium.executablePath;
+async function fetchScreenshot(url) {
+  const endpoint = `https://api.curations.tech/api/screenshot?url=${encodeURIComponent(
+    url
+  )}`;
 
-  if (!executablePath) {
-    // running locally
-    const puppeteer = require("puppeteer");
-    return puppeteer.launch({
-      args: chromium.args,
-      headless: true,
-      defaultViewport: {
-        width: 1280,
-        height: 720,
-      },
-      ignoreHTTPSErrors: true,
-    });
+  try {
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch screenshot: ${response.status}`);
+    }
+
+    const blob = await response.blob();
+    const buffer = await blob.arrayBuffer();
+    const byteArray = new Uint8Array(buffer);
+
+    return byteArray;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
-
-  return chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: {
-      width: 1280,
-      height: 720,
-    },
-    executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
 }
 
 export default async (req, res) => {
-  let browser = null;
   let { imageUrl, name, category, subcategory } = req.body;
 
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
-  name = name.replace(/\s/g, "_");
-  try {
-    browser = await getBrowserInstance();
-    let page = await browser.newPage();
-    await page.goto(imageUrl);
-    const imageBuffer = await page.screenshot();
 
-    const image = "uploaded_on_" + Date.now() + ".jpg";
+  name = name.replace(/[^a-zA-Z0-9]/g, "_").toLowerCase();
+  const image = await fetchScreenshot(imageUrl);
 
-    const { data, error } = await supabase.storage
-      .from("images")
-      .upload(`${name}.webp`, imageBuffer, {
-        cacheControl: "3600",
-        upsert: false,
-        contentType: "image/webp",
-        size: 434,
+  const { data, error } = await supabase.storage
+    .from("images")
+    .upload(`${name}.webp`, image, {
+      cacheControl: "3600",
+      upsert: false,
+      contentType: "image/webp",
+      size: 434,
+      width: 434,
+      height: 700,
+      resize: {
+        fit: "cover",
         width: 434,
         height: 700,
-        resize: {
-          fit: "cover",
-          width: 434,
-          height: 700,
-        },
-      });
+      },
+    });
+
+  if (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error saving screenshot to database." });
+  } else {
+    const { data, error } = await supabase.from("curations").insert([
+      {
+        name: name,
+        category: category.toLowerCase(),
+        design: subcategory.toLowerCase(),
+        image: name + ".webp",
+        favicon: "test",
+      },
+    ]);
 
     if (error) {
       console.error(error);
       res.status(500).json({ message: "Error saving screenshot to database." });
     } else {
       res.status(200).json({ message: "Screenshot saved to database." });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error taking screenshot." });
-  } finally {
-    if (browser !== null) {
-      await browser.close();
     }
   }
 };
