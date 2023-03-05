47ff6b53806f4aea6aee4e93cc5c0f1b37b58083
flooorianflo <florian.kiem@hfg.design>
2023-03-05 15:02:04 +0100

    Components Button

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 7s linear infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
