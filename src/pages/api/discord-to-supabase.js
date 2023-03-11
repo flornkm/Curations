46bac8c13d072818e32a92cd1e2cce891187b4e1
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-03-11 18:04:34 +0100

    adjusted the code to get all messages from the discord channel

--- src/pages/api/discord-to-supabase.js
+++ src/pages/api/discord-to-supabase.js
@@ -9,72 +9,75 @@ const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export default async function handler(req, res) {
-  try {
-    // Create a new Discord client
-    const client = new Discord.Client({
-        intents: [
-          Discord.GatewayIntentBits.Guilds,
-          Discord.GatewayIntentBits.GuildMessages,
-          Discord.GatewayIntentBits.MessageContent
-        ]
-      });
-
-    // When the client is ready, retrieve messages from the #contributions channel
-    client.on('ready', async () => {
-      console.log(`Logged in as ${client.user.tag}!`);
-
-      // Retrieve the #contributions channel
-      const channel = client.channels.cache.find((channel) => channel.name === 'contributions');
-
-      if (!channel) {
-        console.error('Could not find the #contributions channel.');
-        return;
-      }
-
-      // Retrieve all messages from the channel
-      const messages = await channel.messages.fetch();
-
-      //transform timestamp from miliseconds to date and time
-        messages.forEach((message) => {
-            message.createdTimestamp = new Date(message.createdTimestamp).toLocaleString();
+    try {
+        // Create a new Discord client
+        const client = new Discord.Client({
+            intents: [
+                Discord.GatewayIntentBits.Guilds,
+                Discord.GatewayIntentBits.GuildMessages,
+                Discord.GatewayIntentBits.MessageContent
+            ]
         });
 
-      // Transform the messages into an array of objects
-      const messageArray = messages.map((message) => ({
-        link: message.content,  
-        timestamp: message.createdTimestamp,
-      }));
+        // When the client is ready, retrieve messages from the #contributions channel
+        client.on('ready', async () => {
+            console.log(`Logged in as ${client.user.tag}!`);
 
-      console.log(messages);
+            // Retrieve the #contributions channel
+            const channel = client.channels.cache.find((channel) => channel.name === 'contributions');
 
-      // Connect to the Supabase client
-      const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+            if (!channel) {
+                console.error('Could not find the #contributions channel.');
+                return;
+            }
 
-      console.log("1message");
-      console.log(messageArray[0]);
+            // Retrieve all messages from the channel
+            const allMessages = [];
+            let lastMessageId;
+            let messages = await channel.messages.fetch({ limit: 100 });
 
-      // Insert the messages into a Supabase table
-      const { data, error } = await supabase.from('unconfirmed_links').insert(messageArray[0]);
+            while (messages.size > 0) {
+                lastMessageId = messages.last().id;
+                allMessages.push(...messages.values());
+                messages = await channel.messages.fetch({ before: lastMessageId, limit: 100 });
+            }
 
-      if (error) {
-        console.error('Failed to insert messages into the database:', error.message);
-        return;
-      }
+            // Transform the messages into an array of objects
+            const messageArray = allMessages.map((message) => ({
+                link: message.content,
+                timestamp: new Date(message.createdTimestamp).toLocaleString(),
+            }));
 
-      console.log('Inserted', data.length, 'messages into the database.');
+            console.log(messages[0]);
 
-      // Disconnect the Discord client
-      client.destroy();
+            // Connect to the Supabase client
+            const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-      // Return a success response
-      res.status(200).json({ message: 'Messages inserted successfully.' });
-    });
+            console.log("1message");
+            console.log(messageArray[0]);
 
-    // Login to the Discord client
-    await client.login(DISCORD_TOKEN);
-  } catch (error) {
-    console.error('An error occurred:', error);
-    res.status(500).json({ message: 'Failed to insert messages.' });
-  }
+            // Insert the messages into a Supabase table
+            const { data, error } = await supabase.from('unconfirmed_links').insert(messageArray);
+
+            if (error) {
+                console.error('Failed to insert messages into the database:', error.message);
+                return;
+            }
+
+            console.log('Inserted', data.length, 'messages into the database.');
+
+            // Disconnect the Discord client
+            client.destroy();
+
+            // Return a success response
+            res.status(200).json({ message: 'Messages inserted successfully.' });
+        });
+
+        // Login to the Discord client
+        await client.login(DISCORD_TOKEN);
+    } catch (error) {
+        console.error('An error occurred:', error);
+        res.status(500).json({ message: 'Failed to insert messages.' });
+    }
 }
 
