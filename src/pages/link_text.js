022ff234647ff050449f35c0253b67fc0ece523e
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-03-19 14:52:59 +0100

    working on styling of the dashboard page

--- src/pages/link_text.js
+++ src/pages/link_text.js
@@ -1,9 +1,16 @@
 import UnconfirmedLink from '../components/UnconfirmedLink'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <UnconfirmedLink />
+      <div className="flex flex-col justify-center items-start w-screen p-1 h-auto border-b-2 border-zinc-800">
+        <Image src="/images/curations_logo.png" alt="Curations Logo" width={120} height={24} />
+      </div>
+        <div className="flex flex-col justify-center items-center w-auto h-100">
+          <p className="text-2xl font-bold">Unconfirmed Links</p>
+        </div>
+        <UnconfirmedLink />
     </div>
   )
 }
