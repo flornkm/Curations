a14920ae985381befd14494c9ff6b7e0cb5d4eb7
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-03-19 15:11:57 +0100

    working on tailwind classes

--- src/pages/link_text.js
+++ src/pages/link_text.js
@@ -4,13 +4,17 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-start w-screen p-1 h-auto border-b-2 border-zinc-800">
+      <div className="flex flex-col justify-center items-start p-1 w-screen h-auto border-b-2 border-zinc-800">
         <Image src="/images/curations_logo.png" alt="Curations Logo" width={120} height={24} />
       </div>
-        <div className="flex flex-col justify-center items-center w-auto h-100">
-          <p className="text-2xl font-bold">Unconfirmed Links</p>
+      <div className="flex flex-row justfiy-start items-start w-100 h-100">
+        <div className="flex flex-col justify-start items-center w-auto h-100 border-r-2 border-zinc-800">
+          <div className="flex flex-col justify-start items-center p-1 w-auto h-auto border-b-2 border-zinc-800">
+            <p className="text-base font-semibold">Unconfirmed Links</p>
+          </div>
         </div>
         <UnconfirmedLink />
+      </div>
     </div>
   )
 }
