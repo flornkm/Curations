79f4f29b6b069db7076628d0c10d5bd7a09faa32
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-03-20 09:20:45 +0100

    working on admin panel

--- src/pages/link_text.js
+++ src/pages/link_text.js
@@ -4,16 +4,23 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-start p-1 w-screen h-auto border-b-2 border-zinc-800">
-        <Image src="/images/curations_logo.png" alt="Curations Logo" width={120} height={24} />
+      <div className="flex flex-col justify-center items-start p-4 w-screen h-auto border-b-2 border-zinc-800">
+        <Image src="../images/curations_logo.png" alt="Curations Logo" width={120} height={24} />
       </div>
       <div className="flex flex-row justfiy-start items-start w-100 h-100">
-        <div className="flex flex-col justify-start items-center w-auto h-100 border-r-2 border-zinc-800">
-          <div className="flex flex-col justify-start items-center p-1 w-auto h-auto border-b-2 border-zinc-800">
-            <p className="text-base font-semibold">Unconfirmed Links</p>
+        <div className="flex flex-col justify-start items-center w-auto h-screen border-r-2 border-zinc-800">
+          <div className="flex flex-col justify-center items-start p-4 gap-1 w-auto h-auto border-b-2 border-zinc-800">
+            <p className="text-base font-semibold">Admin Panel</p>
+            <p className="text-sm font-regular text-zinc-500">nils.eller@hfg.design</p>
           </div>
         </div>
-        <UnconfirmedLink />
+        <div className="flex flex-col justify-start items-start w-100 h-100">
+          <div className="flex flex-col justify-center items-start p-4 gap-1 w-screen h-auto border-b-2 border-zinc-800">
+            <p className="text-sm font-regular text-zinc-500">Curations / Link list</p>
+            <p className="text-base font-semibold">Link list</p>
+          </div>
+          <UnconfirmedLink />
+        </div>
       </div>
     </div>
   )
