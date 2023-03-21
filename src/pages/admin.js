6002f9ed7528572e1a03767f4776b42bcff9f049
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-03-21 19:19:47 +0100

    working on menu panel stylings

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -15,18 +15,25 @@ export default function Home() {
 
       <div className="flex flex-row justfiy-start items-start w-100 h-100">
 
-        <div className="flex flex-col justify-start items-start w-auto h-[calc(100vh-2rem)] border-r-2 border-zinc-800">
-          <div className="flex flex-col justify-center items-start p-4 gap-1 w-auto h-auto border-b-2 border-zinc-800">
+        <div className="flex flex-col justify-start items-start min-w-100 h-[calc(100vh-2rem)] whitespace-nowrap border-r-2 border-zinc-800">
+          <div className="flex flex-col justify-center items-start p-4 gap-1 w-full h-auto border-b-2 border-zinc-800">
             <p className="text-base font-semibold">Admin Panel</p>
             <p className="text-sm font-regular text-zinc-500">
               nils.eller@hfg.design
             </p>
           </div>
           
-          <div className="flex flex-col justify-start items-start p-4 gap-1 w-auto h-100">
-            <p className="text-base font-semibold">Link list</p>
-            <p className="text-base font-semibold">Link list</p>
-            <p className="text-base font-semibold">Link list</p>
+          <div className="flex flex-col justify-start items-start p-4 gap-2 w-auto h-100">
+            <div className="flex px-4 py-1 border-l-2 border-white">
+              <p className="text-base font-semibold">Link list</p>
+            </div>
+            <div className="flex px-4 py-1">
+              <p className="text-base font-semibold">Add link</p>
+            </div>
+            <div className="flex px-4 py-1">
+              <p className="text-base font-semibold">Add component</p>
+            </div>
+            
           </div>
         </div>
 
