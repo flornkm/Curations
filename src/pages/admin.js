dabfd7f6905b9f6dee0d4a715e1f4b4ab2405cb3
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-03-21 19:08:01 +0100

    working on admin panel layout

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,32 +1,48 @@
-import UnconfirmedLink from '../components/UnconfirmedLink'
-import Image from 'next/image'
+import UnconfirmedLink from "../components/UnconfirmedLink";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex flex-col justify-center items-start p-4 w-full h-auto border-b-2 border-zinc-800">
-        <Image src="../images/curations_logo.png" alt="Curations Logo" width={120} height={24} />
-      </div>
+    <div className="w-screen h-screen overflow-hidden overflow-x-hidden">
+        <div className="flex flex-col justify-center items-start p-4 w-full h-auto border-b-2 border-zinc-800">
+          <Image
+            src="/images/curations_logo.png"
+            alt="Curations Logo"
+            width={120}
+            height={24}
+          />
+        </div>
+
       <div className="flex flex-row justfiy-start items-start w-100 h-100">
-        <div className="flex flex-col justify-start items-center w-auto h-100 border-r-2 border-zinc-800">
+
+        <div className="flex flex-col justify-start items-start w-auto h-[calc(100vh-2rem)] border-r-2 border-zinc-800">
           <div className="flex flex-col justify-center items-start p-4 gap-1 w-auto h-auto border-b-2 border-zinc-800">
             <p className="text-base font-semibold">Admin Panel</p>
-            <p className="text-sm font-regular text-zinc-500">nils.eller@hfg.design</p>
+            <p className="text-sm font-regular text-zinc-500">
+              nils.eller@hfg.design
+            </p>
           </div>
-          <div className="flex flex-col justify-start items-center w-auto h-100">
+          
+          <div className="flex flex-col justify-start items-start p-4 gap-1 w-auto h-100">
             <p className="text-base font-semibold">Link list</p>
             <p className="text-base font-semibold">Link list</p>
             <p className="text-base font-semibold">Link list</p>
           </div>
         </div>
+
         <div className="flex flex-col justify-start items-start w-100 h-100 overflow-hidden">
           <div className="flex flex-col justify-center items-start p-4 gap-1 w-full h-auto border-b-2 border-zinc-800">
-            <p className="text-sm font-regular text-zinc-500">Curations / Link list</p>
+            <p className="text-sm font-regular text-zinc-500">
+              Curations / Link list
+            </p>
             <p className="text-base font-semibold">Link list</p>
           </div>
-          <UnconfirmedLink />
+          <div className="h-[calc(100vh-8rem)] overflow-y-auto">
+            <UnconfirmedLink />
+          </div>
         </div>
       </div>
+
     </div>
-  )
+  );
 }
