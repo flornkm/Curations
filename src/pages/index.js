766d3731f6e518a86f96c4c3d60770dba90eda34
flooorianflo <florian.kiem@hfg.design>
2023-04-02 21:46:01 +0200

    Added Async / Await

--- src/pages/index.js
+++ src/pages/index.js
@@ -85,17 +85,20 @@ export default function Curations() {
       .catch((error) => console.log(error));
   };
 
-  const loadSubcategoryItems = (category, itemName) => {
+  const loadSubcategoryItems = async (category, itemName) => {
     main.current.style.pointerEvents = "none";
     setLoading(true);
-    fetch("/api/supabase?category=" + category + "&subCategory=" + itemName)
-      .then((res) => res.json())
-      .then((data) => {
-        main.current.style.pointerEvents = "auto";
-        setLoading(false);
-        setItems(data);
-      })
-      .catch((error) => console.log(error));
+    try {
+      const response = await fetch(
+        "/api/supabase?category=" + category + "&subCategory=" + itemName
+      );
+      const data = await response.json();
+      main.current.style.pointerEvents = "auto";
+      setLoading(false);
+      setItems(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -159,35 +162,35 @@ export default function Curations() {
           (!loading && items && (
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center md:px-8 max-md:pt-12">
               {items.map((item) => (
-                  <Link
-                    target="_blank"
-                    key={item.id}
-                    href={item.link}
-                    className="rounded-xl shadow-lg flex flex-col gap-2 transition-all hover:scale-[0.98] focus:outline-1 focus:outline-red-300"
-                  >
-                    {item.image && (
+                <Link
+                  target="_blank"
+                  key={item.id}
+                  href={item.link}
+                  className="rounded-xl shadow-lg flex flex-col gap-2 transition-all hover:scale-[0.98] focus:outline-1 focus:outline-red-300"
+                >
+                  {item.image && (
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      width={500}
+                      height={500}
+                      className="rounded-lg"
+                    />
+                  )}
+                  <div className="flex gap-4 place-items-center">
+                    {item.favicon && (
                       <Image
-                        src={item.image}
-                        alt={item.title}
-                        width={500}
-                        height={500}
-                        className="rounded-lg"
+                        src={item.favicon}
+                        alt={item.title + " favicon"}
+                        width={16}
+                        height={16}
+                        className="object-contain"
                       />
                     )}
-                    <div className="flex gap-4 place-items-center">
-                      {item.favicon && (
-                        <Image
-                          src={item.favicon}
-                          alt={item.title + " favicon"}
-                          width={16}
-                          height={16}
-                          className="object-contain"
-                        />
-                      )}
-                      <h3 className="font-medium">{item.name}</h3>
-                    </div>
-                  </Link>
-                ))}
+                    <h3 className="font-medium">{item.name}</h3>
+                  </div>
+                </Link>
+              ))}
             </div>
           ))}
         <About sidebarWrapper={sidebarWrapper} plusIcon={plusIcon} />
