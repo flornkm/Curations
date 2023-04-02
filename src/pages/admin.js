dfca7026165e0da12cc036c1dc35decd9274a27d
flooorianflo <florian.kiem@hfg.design>
2023-04-02 13:44:52 +0200

    User Login Working

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -14,6 +14,7 @@ export default function Home() {
   const [showAddLinkPage, setShowAddLinkPage] = useState(false);
   const [itemData, setItemData] = useState({});
   const [data, setData] = useState([]);
+  const [userRank, setUserRank] = useState(null);
 
   const session = useSession()
   const supabase = useSupabaseClient()
@@ -65,9 +66,26 @@ export default function Home() {
     }
   }
 
+  async function lookUserRank(session) {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", session.user.id);
+    if (error) console.log(error);
+    else {
+      setUserRank(data[0].rank);
+    }
+  }
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (session) {
+      lookUserRank(session);
+    }
+  }, [session]);
 
   return (
     !session ? (
@@ -75,111 +93,117 @@ export default function Home() {
         <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
       </div>
     ) : (
-      <div className="w-screen h-screen overflow-hidden overflow-x-hidden">
-        {showDeleteModal ? (
-          <DeleteLinkModal
-            onCloseModal={handleShowDeleteModal}
-            itemData={itemData}
-            fetchData={fetchData}
-          />
-        ) : null}
-        {showEditModal ? (
-          <AddLinkModal
-            onCloseModal={handleShowEditModal}
-            itemData={itemData}
-            fetchData={fetchData}
-          />
-        ) : null}
-        <div className="flex flex-col justify-center items-start p-4 w-full h-auto border-b-2 border-zinc-800">
-          <Image
-            src="/images/curations_logo.png"
-            alt="Curations Logo"
-            width={120}
-            height={24}
-            loader={({ src }) => src}
-          />
-        </div>
+      userRank === "admin" ? (
+        <div className="w-screen h-screen overflow-hidden overflow-x-hidden">
+          {showDeleteModal ? (
+            <DeleteLinkModal
+              onCloseModal={handleShowDeleteModal}
+              itemData={itemData}
+              fetchData={fetchData}
+            />
+          ) : null}
+          {showEditModal ? (
+            <AddLinkModal
+              onCloseModal={handleShowEditModal}
+              itemData={itemData}
+              fetchData={fetchData}
+            />
+          ) : null}
+          <div className="flex flex-col justify-center items-start p-4 w-full h-auto border-b-2 border-zinc-800">
+            <Image
+              src="/images/curations_logo.png"
+              alt="Curations Logo"
+              width={120}
+              height={24}
+              loader={({ src }) => src}
+            />
+          </div>
 
-        <div className="flex flex-row justfiy-start items-start w-100 h-100">
-          <div className="flex flex-col justify-start items-start min-w-100 h-[calc(100vh-2rem)] whitespace-nowrap border-r-2 border-zinc-800">
-            <div className="flex flex-col justify-center items-start p-4 gap-1 w-full h-auto border-b-2 border-zinc-800">
-              <p className="text-base font-semibold">Admin Panel</p>
-              <p className="text-sm font-regular text-zinc-500">
-                {session.user.email}
-              </p>
-            </div>
+          <div className="flex flex-row justfiy-start items-start w-100 h-100">
+            <div className="flex flex-col justify-start items-start min-w-100 h-[calc(100vh-2rem)] whitespace-nowrap border-r-2 border-zinc-800">
+              <div className="flex flex-col justify-center items-start p-4 gap-1 w-full h-auto border-b-2 border-zinc-800">
+                <p className="text-base font-semibold">Admin Panel</p>
+                <p className="text-sm font-regular text-zinc-500">
+                  {session.user.email}
+                </p>
+              </div>
 
-            <div className="flex flex-col justify-start items-start p-4 gap-2 w-auto h-100">
-              <div
-                className={
-                  showLinkListPage
-                    ? "flex px-4 py-1 border-l-2 border-white"
-                    : "flex px-4 py-1"
-                }
-              >
-                <button
-                  onClick={handleLinkListPage}
+              <div className="flex flex-col justify-start items-start p-4 gap-2 w-auto h-100">
+                <div
                   className={
                     showLinkListPage
-                      ? "text-base font-semibold text-zinc-200"
-                      : "text-base font-semibold text-zinc-600 hover:text-zinc-500"
+                      ? "flex px-4 py-1 border-l-2 border-white"
+                      : "flex px-4 py-1"
                   }
                 >
-                  Link list
-                </button>
-              </div>
-              <div
-                className={
-                  showAddLinkPage
-                    ? "flex px-4 py-1 border-l-2 border-white"
-                    : "flex px-4 py-1"
-                }
-              >
-                <button
-                  onClick={handleAddLinkPage}
+                  <button
+                    onClick={handleLinkListPage}
+                    className={
+                      showLinkListPage
+                        ? "text-base font-semibold text-zinc-200"
+                        : "text-base font-semibold text-zinc-600 hover:text-zinc-500"
+                    }
+                  >
+                    Link list
+                  </button>
+                </div>
+                <div
                   className={
                     showAddLinkPage
-                      ? "text-base font-semibold text-zinc-200"
-                      : "text-base font-semibold text-zinc-600 hover:text-zinc-500"
+                      ? "flex px-4 py-1 border-l-2 border-white"
+                      : "flex px-4 py-1"
                   }
                 >
-                  Add Link
-                </button>
-              </div>
-              <div className="flex px-4 py-1">
-                <button
-                  onClick={() => supabase.auth.signOut()}
-                  className="text-base font-semibold text-zinc-600 hover:text-zinc-500"
-                >
-                  Log out
-                </button>
+                  <button
+                    onClick={handleAddLinkPage}
+                    className={
+                      showAddLinkPage
+                        ? "text-base font-semibold text-zinc-200"
+                        : "text-base font-semibold text-zinc-600 hover:text-zinc-500"
+                    }
+                  >
+                    Add Link
+                  </button>
+                </div>
+                <div className="flex px-4 py-1">
+                  <button
+                    onClick={() => supabase.auth.signOut()}
+                    className="text-base font-semibold text-zinc-600 hover:text-zinc-500"
+                  >
+                    Log out
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="flex flex-col justify-start items-start w-100 h-100 overflow-hidden">
-            <div className="flex flex-col justify-center items-start p-4 gap-1 w-screen h-auto border-b-2 border-zinc-800">
-              <p className="text-sm font-regular text-zinc-500">
-                {showAddLinkPage ? "Curations / Add Link" : null}
-                {showLinkListPage ? "Curations / Link List" : null}
-              </p>
-              <p className="text-base font-semibold">
-                {showAddLinkPage ? "Add Link" : null}
-                {showLinkListPage ? "Link List" : null}
-              </p>
-            </div>
-            <div className="h-[calc(100vh-8rem)] overflow-y-auto">
-              {showLinkListPage ? (
-                <UnconfirmedLink
-                  onShowEditModal={handleShowEditModal}
-                  onShowDeleteModal={handleShowDeleteModal}
-                  onItemData={handleItemData}
-                  data={data}
-                />
-              ) : null}
+            <div className="flex flex-col justify-start items-start w-100 h-100 overflow-hidden">
+              <div className="flex flex-col justify-center items-start p-4 gap-1 w-screen h-auto border-b-2 border-zinc-800">
+                <p className="text-sm font-regular text-zinc-500">
+                  {showAddLinkPage ? "Curations / Add Link" : null}
+                  {showLinkListPage ? "Curations / Link List" : null}
+                </p>
+                <p className="text-base font-semibold">
+                  {showAddLinkPage ? "Add Link" : null}
+                  {showLinkListPage ? "Link List" : null}
+                </p>
+              </div>
+              <div className="h-[calc(100vh-8rem)] overflow-y-auto">
+                {showLinkListPage ? (
+                  <UnconfirmedLink
+                    onShowEditModal={handleShowEditModal}
+                    onShowDeleteModal={handleShowDeleteModal}
+                    onItemData={handleItemData}
+                    data={data}
+                  />
+                ) : null}
+              </div>
             </div>
           </div>
+        </div>) : (
+        <div className="max-w-xl mx-auto pt-10">
+          <h1 className="text-2xl font-semibold">You are not authorized to access this page.</h1>
         </div>
-      </div>)
+      )
+    )
   );
 }
