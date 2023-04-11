e37ac2c34160dcb881acff512af2c0ccedc2487f
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2023-04-11 22:04:51 +0200

    worked in notion-to-supabase script

--- src/pages/api/notion-to-supabase.js
+++ src/pages/api/notion-to-supabase.js
@@ -2,15 +2,16 @@ import { Client } from '@notionhq/client';
 import { createClient } from '@supabase/supabase-js';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
-const databaseId = 'YOUR_DATABASE_ID';
-const tableName = 'YOUR_TABLE_NAME';
+const databaseId = '0bf14ccad4f9463cb1897ad77a6b4d0f';
+const tableName = 'Curation Archive';
 
 export default async function handler(req, res) {
   try {
     // Retrieve all pages from the Notion database
     const { results } = await notion.databases.query({ database_id: databaseId });
+    console.log(results);
 
     // Extract links from each page and format them for import into Supabase
     const data = results.map((page) => ({
@@ -19,7 +20,7 @@ export default async function handler(req, res) {
 
     // Import the data into Supabase
     // const { data: importedData, error } = await supabase.from('unconfirmed_links').insert(data);
-    console.log(data);
+    // console.log(data);
 
     if (error) {
       console.error(error);
